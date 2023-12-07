13fc58fea28cb403a804239ee05987e0c8c47274
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-07 13:40:35 +0100

    add strict mode

---
+++ section-05-project-01-es-6-features/app.js
@@ -0,0 +1,39 @@
+// let firstName = 'Max'
+
+// function greet(){
+//     let age = 30
+//     let firstName = 'Alex'
+//     console.log(firstName, age)
+// }
+
+// console.log(firstName)
+
+// greet()
+
+// 'use strict' // use strict mode
+
+var firstName = 'Max'
+
+if (firstName === 'Max'){
+    var hobbies = ['GYM','Boxing']
+    console.log(hobbies)
+}
+
+function greet(){
+    var age = 30
+    var firstName = 'Alex'
+    console.log(firstName, age)
+}
+
+console.log(firstName, hobbies)
+
+greet()
+
+console.log(lastName)
+
+var lastName = 'Anderson'
+
+// ''
+// "someText"
+// surName = 'something'
+
