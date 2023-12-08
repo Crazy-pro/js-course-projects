edc99867b202ae078febb1f203ea4810278006d1
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-08 16:40:06 +0100

    add user choice

--- section-06-project-01-functions/app.js
+++ section-06-project-01-functions/app.js
@@ -1,8 +1,28 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
-const start = function startGame() {
-    console.log('Game is starting...')
+const ROCK = 'ROCK'
+const PAPER = 'PAPER'
+const SCISSORS = 'SCISSORS'
+const DEFAULT_USER_CHOICE = ROCK
+
+let gameIsRunning = false
+
+const getPlayerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase()
+    if(
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ){
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for u!`)
+        return DEFAULT_USER_CHOICE
+    }
+    return selection
 }
+
+// const start = function startGame() {
+//     console.log('Game is starting...')
+// }
 
 // const person = {
 //     name: 'Max',
@@ -22,4 +42,12 @@ const start = function startGame() {
 //     console.log('Game is starting...')
 // })
 
-startGameBtn.addEventListener('click', start)
+startGameBtn.addEventListener('click', function() {
+    if(gameIsRunning){
+        return
+    }
+    gameIsRunning = true
+    console.log('Game is starting...')
+    const playerSelection = getPlayerChoice()
+    console.log(playerSelection)
+})
