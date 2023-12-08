40efe8d5e409628442df6a6984b8ef1d56a5afae
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-08 16:57:24 +0100

    add computer choice and get winner functions

--- section-06-project-01-functions/app.js
+++ section-06-project-01-functions/app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK'
 const PAPER = 'PAPER'
 const SCISSORS = 'SCISSORS'
 const DEFAULT_USER_CHOICE = ROCK
+const RESULT_DRAW = 'DRAW'
+const RESULT_PLAYER_WINS = 'PLAYER_WINS'
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS'
 
 let gameIsRunning = false
 
@@ -19,6 +22,43 @@ const getPlayerChoice = function() {
     }
     return selection
 }
+
+const getComputerChoice = function() {
+    const randomChoice = Math.random()
+    if(randomChoice < 0.34) {
+        return ROCK
+    } else if(randomChoice < 0.67) {
+        return PAPER
+    } else {
+        return SCISSORS
+    }
+}
+
+const getWinner = (playerChoice, computerChoice) => {
+    if(playerChoice === computerChoice) {
+        return RESULT_DRAW
+    } else if(playerChoice === ROCK && computerChoice === SCISSORS ||
+              playerChoice === PAPER && computerChoice === ROCK ||
+              playerChoice === SCISSORS && computerChoice === PAPER) {
+        return RESULT_PLAYER_WINS
+    } else {
+        return RESULT_COMPUTER_WINS
+    }
+}
+
+startGameBtn.addEventListener('click', function() {
+    if(gameIsRunning){
+        return
+    }
+    gameIsRunning = true
+    console.log('Game is starting...')
+    const playerChoice = getPlayerChoice()
+    const computerChoice = getComputerChoice()
+    const winner = getWinner(playerChoice, computerChoice)
+    console.log(winner)
+})
+
+
 
 // const start = function startGame() {
 //     console.log('Game is starting...')
@@ -41,13 +81,3 @@ const getPlayerChoice = function() {
 // startGameBtn.addEventListener('click', function() {
 //     console.log('Game is starting...')
 // })
-
-startGameBtn.addEventListener('click', function() {
-    if(gameIsRunning){
-        return
-    }
-    gameIsRunning = true
-    console.log('Game is starting...')
-    const playerSelection = getPlayerChoice()
-    console.log(playerSelection)
-})
