4802e89e96c6a5fd939e7029c3086a33ec3a1225
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-10 15:54:06 +0100

    add dom basics methods

--- section-07-project-01-dom/app.js
+++ section-07-project-01-dom/app.js
@@ -7,6 +7,12 @@ h1.textContent = 'New text'
 h1.style.color = 'red'
 h1.style.backgroundColor = 'orange'
 
+const li = document.querySelector('li:last-of-type')
+li.textContent = li.textContent + ' (Changed!)'
+
+const body = document.body
+body.querySelector('header') 
+
 const listItemElements = document.getElementsByTagName('li')
 
 for (const listItemEl of listItemElements) {
