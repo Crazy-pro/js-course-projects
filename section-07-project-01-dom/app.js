149cb260ade7c9aaccd2bd3c02b7a408066c66c4
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-10 15:51:47 +0100

    add doc getelement by id

--- section-07-project-01-dom/app.js
+++ section-07-project-01-dom/app.js
@@ -1,2 +1,14 @@
-/*window.*/alert()
-console.log(/*window.*/document)
+// /*window.*/alert()
+// console.log(/*window.*/document)
+
+const h1 = document.getElementById('main-title')
+
+h1.textContent = 'New text'
+h1.style.color = 'red'
+h1.style.backgroundColor = 'orange'
+
+const listItemElements = document.getElementsByTagName('li')
+
+for (const listItemEl of listItemElements) {
+    console.dir(listItemEl)
+}
