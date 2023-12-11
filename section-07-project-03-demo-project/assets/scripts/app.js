be09c4658cb23870d1d84eace6d7a91937366591
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 16:01:03 +0100

    add addMovieHandler

--- section-07-project-03-demo-project/assets/scripts/app.js
+++ section-07-project-03-demo-project/assets/scripts/app.js
@@ -1,14 +1,12 @@
 // const addMovieModal = document.body.childre[1]
 // const addMovieModal = document.querySelector('#add-modal')
 const addMovieModal = document.getElementById('add-modal')
-
 // const startAddMovieButton = document.querySelector('header').lastElementChild
 const startAddMovieButton = document.querySelector('header button')
-
 // const backdrop = document.body.firstElementChild
 const backdrop = document.getElementById('backdrop')
-
 const cancelAddMovieButton = addMovieModal.querySelector('.btn---passive')
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
@@ -23,10 +21,15 @@ const backdropClickHandler = () => {
     toggleMovieModal()
 }
 
-const cancelAddMovie = () => {
+const cancelMovieHandler = () => {
+    toggleMovieModal()
+}
+
+const addMovieHandler = () => {
     toggleMovieModal()
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
 backdrop.addEventListener('click', backdropClickHandler)
-cancelAddMovieButton.addEventListener('click', cancelAddMovie)
+cancelAddMovieButton.addEventListener('click', cancelMovieHandler)
+confirmAddMovieButton.addEventListener('click', addMovieHandler)
