5a29f3b15ee61be646ebe7a732c8dcd8a9898053
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 15:57:26 +0100

    add cancelAddMovie

--- section-07-project-03-demo-project/assets/scripts/app.js
+++ section-07-project-03-demo-project/assets/scripts/app.js
@@ -8,6 +8,8 @@ const startAddMovieButton = document.querySelector('header button')
 // const backdrop = document.body.firstElementChild
 const backdrop = document.getElementById('backdrop')
 
+const cancelAddMovieButton = addMovieModal.querySelector('.btn---passive')
+
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
 }
@@ -21,5 +23,10 @@ const backdropClickHandler = () => {
     toggleMovieModal()
 }
 
+const cancelAddMovie = () => {
+    toggleMovieModal()
+}
+
 startAddMovieButton.addEventListener('click', toggleMovieModal)
 backdrop.addEventListener('click', backdropClickHandler)
+cancelAddMovieButton.addEventListener('click', cancelAddMovie)
