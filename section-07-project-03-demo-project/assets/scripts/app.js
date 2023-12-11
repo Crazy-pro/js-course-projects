a1cbc63f9bd44a33264c261d789c0d68a87b8d28
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 15:46:39 +0100

    add selectors

---
+++ section-07-project-03-demo-project/assets/scripts/app.js
@@ -0,0 +1,7 @@
+// const addMovieModal = document.body.childre[1]
+// const addMovieModal = document.querySelector('#add-modal')
+const addMovieModal = document.getElementById('add-modal')
+
+// const startAddMovieButton = document.querySelector('header').lastElementChild
+const startAddMovieButton = document.querySelector('header button')
+
