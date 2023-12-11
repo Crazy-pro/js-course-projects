52478c392bd82f05e685d0b8c3041c488e41ee37
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 15:55:11 +0100

    add backdropClickHandler

--- section-07-project-03-demo-project/assets/scripts/app.js
+++ section-07-project-03-demo-project/assets/scripts/app.js
@@ -5,9 +5,21 @@ const addMovieModal = document.getElementById('add-modal')
 // const startAddMovieButton = document.querySelector('header').lastElementChild
 const startAddMovieButton = document.querySelector('header button')
 
+// const backdrop = document.body.firstElementChild
+const backdrop = document.getElementById('backdrop')
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible')
+}
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible')
+    toggleBackdrop()
+}
+
+const backdropClickHandler = () => {
+    toggleMovieModal()
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
+backdrop.addEventListener('click', backdropClickHandler)
