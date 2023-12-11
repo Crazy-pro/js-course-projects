30f4e257160ee805359b6e4719ce133d958a7e8b
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 17:23:36 +0100

    add remove

--- section-07-project-03-demo-project/assets/scripts/app.js
+++ section-07-project-03-demo-project/assets/scripts/app.js
@@ -5,11 +5,52 @@ const addMovieModal = document.getElementById('add-modal')
 const startAddMovieButton = document.querySelector('header button')
 // const backdrop = document.body.firstElementChild
 const backdrop = document.getElementById('backdrop')
-const cancelAddMovieButton = addMovieModal.querySelector('.btn---passive')
-const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const cancelAddMovieButton = document.querySelector('.btn--passive')
+// const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const confirmAddMovieButton = document.querySelector('.btn--success')
 // const userInputs = addMovieModal.getElementsByTagName('input')
 const userInputs = addMovieModal.querySelectorAll('input')
+const entryTextSection = document.getElementById('entry-text')
+const movies = []
 
+const updateUI = () => {
+    if(movies.length === 0){
+        entryTextSection.style.display = 'block'
+    } else {
+        entryTextSection.style.display = 'none'
+    }
+}
+
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0
+    for(const movie of movies){
+        if(movie.id === movieId){
+            break
+        }
+        movieIndex++
+    }
+    movies.splice(movieIndex, 1)
+    const rootList = document.getElementById('movie-list')
+    rootList.children[movieIndex].remove()
+    // rootList.removeChild(rootList.children[movieIndex])
+}
+
+const renderNewMovie = (movie) => {
+    const newMovieElement = document.createElement('li')
+    newMovieElement.className = 'movie-element'
+    newMovieElement.insertAdjacentHTML = `
+    <div class="movie-element__image">
+        <img src="${movie.image}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+        <h2>${movie.title}</h2>
+        <p>${movie.rating}/ 5 stars</p>
+    </div>
+    `
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id))
+    const rootList = document.getElementById('movie-list')
+    rootList.append(newMovieElement)
+}
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
@@ -26,6 +67,13 @@ const backdropClickHandler = () => {
 
 const cancelMovieHandler = () => {
     toggleMovieModal()
+    clearInputs()
+}
+
+const clearInputs = () => {
+    for(const userInput of userInputs){
+        userInput.value = ''
+    }
 }
 
 const addMovieHandler = () => {
@@ -43,6 +91,20 @@ const addMovieHandler = () => {
         alert('Please enter valid values (rating should be between 1 and 5!)')
         return
     }
+
+    const newMovie = {
+        id: Math.random().toString(),
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue
+    }
+
+    movies.push(newMovie)
+    console.log(movies)
+    toggleMovieModal()
+    clearInputs()
+    renderNewMovie(newMovie)
+    updateUI()
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
