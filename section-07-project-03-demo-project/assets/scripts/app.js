69e834badff92c06c1d2aa34e218a9a155a8b385
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 16:06:42 +0100

    add validation

--- section-07-project-03-demo-project/assets/scripts/app.js
+++ section-07-project-03-demo-project/assets/scripts/app.js
@@ -7,6 +7,9 @@ const startAddMovieButton = document.querySelector('header button')
 const backdrop = document.getElementById('backdrop')
 const cancelAddMovieButton = addMovieModal.querySelector('.btn---passive')
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+// const userInputs = addMovieModal.getElementsByTagName('input')
+const userInputs = addMovieModal.querySelectorAll('input')
+
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible')
@@ -26,7 +29,20 @@ const cancelMovieHandler = () => {
 }
 
 const addMovieHandler = () => {
-    toggleMovieModal()
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if(
+        titleValue.trim() === '' || 
+        imageUrlValue === '' || 
+        ratingValue === '' ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert('Please enter valid values (rating should be between 1 and 5!)')
+        return
+    }
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
