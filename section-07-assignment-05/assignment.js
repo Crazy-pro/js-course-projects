035336ee63aacb47115d108f4615d29411b540ca
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-11 12:38:43 +0100

    add assignment 5

---
+++ section-07-assignment-05/assignment.js
@@ -0,0 +1,18 @@
+// Task 1:
+const element1 = document.getElementById('task-1')
+const element2 = document.querySelector('li')
+
+element1.style.color = 'black'
+element2.style.backgroundColor = 'black'
+
+// Task 2:
+const element3 = document.querySelector('title')
+element3.textContent = 'Assignment solved'
+
+const element4 = document.head
+const element5 = document.querySelector('title')
+element5.textContent = 'Assignment also solved'
+
+// const h1 = document.querySelector('h1')
+const h1 = document.getElementsByTagName('h1')
+h1[0].textContent = 'Assignment - solved'
