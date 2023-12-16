d68be887342cf7bf8bdb835cb84c96bdea6c9293
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-16 16:50:13 +0100

    add key properties using []

--- section-09-project-01-objects/assets/scripts/objects.js
+++ section-09-project-01-objects/assets/scripts/objects.js
@@ -3,10 +3,14 @@ const movies = document.getElementById('movie-list')
 movies.style['background-color'] = 'red'
 movies.style.display = 'block'
 
+
+const userChosenKeyName = 'acces lvl'
+
 const person = {
   'first Name': 'Alex',
   'age': 25,
   hobbies: ['Sports', 'Cooking'],
+  [userChosenKeyName]: '...',
   greet: function() {
     alert('Hi there!');
   },
@@ -20,6 +24,8 @@ person.age = 26
 // person.age = null
 person.greet();
 
+const keyName = 'first Name'
 console.log(person)
-console.log(person['first Name'])
+console.log(person[keyName])
+console.log(person.keyName)
 console.log(person[1.5])
