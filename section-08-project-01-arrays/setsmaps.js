479416060b7694705434a9e54519743e609ec798
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-16 15:08:42 +0100

    add Set

---
+++ section-08-project-01-arrays/setsmaps.js
@@ -0,0 +1,14 @@
+// const ids = new Set([1, 2, 3])
+// ids.add(1)
+// console.log(ids)
+// console.log(ids.has(1))
+
+// if (ids.has(2)){
+//     console.log(ids.has(2))
+//     ids.delete(2)
+// }
+
+// for (const entry of ids.entries()) {
+//     console.log(entry)
+// }
+
