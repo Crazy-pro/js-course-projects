d488c09d44ac43821c2298efa169cb7ecabd3a40
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-16 13:45:50 +0100

    add for-of for arrays

--- section-08-project-01-arrays/app.js
+++ section-08-project-01-arrays/app.js
@@ -1,15 +1,27 @@
-const numbers = [1, 2, 3]
-const nums = new Array('1','Hi')
-const fixedSizeArray = new Array(7)
-const array = Array.of(1, 4, 5, 7)
-const arr = Array.from('Hello world')
-const listItems = document.querySelectorAll('li')
-const arrayFrom = Array.from(listItems)
+// const numbers = [1, 2, 3]
+// const nums = new Array('1','Hi')
+// const fixedSizeArray = new Array(7)
+// const array = Array.of(1, 4, 5, 7)
+// const arr = Array.from('Hello world')
+// const listItems = document.querySelectorAll('li')
+// const arrayFrom = Array.from(listItems)
 
-console.log(numbers)
-console.log(nums)
-console.log(fixedSizeArray)
-console.log(array)
-console.log(arr)
-console.log(listItems)
-console.log(arrayFrom)
+// console.log(numbers)
+// console.log(nums)
+// console.log(fixedSizeArray)
+// console.log(array)
+// console.log(arr)
+// console.log(listItems)
+// console.log(arrayFrom)
+
+// const hobbies = ['Cooking', 'Sports']
+// const personalData = [30, 'Alex', {moreDetails: []}]
+// const analyticsData =[[-1.5,], [1, 5, 7], 'String', {5: 'Hi'}]
+
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint)
+//     }
+// }
+
+// console.log(personalData[1])
