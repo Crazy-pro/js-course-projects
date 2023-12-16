cd66cc7cf21c504ebc0e033cfc7fc8004894041d
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-16 13:41:49 +0100

    add array basics

---
+++ section-08-project-01-arrays/app.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3]
+const nums = new Array('1','Hi')
+const fixedSizeArray = new Array(7)
+const array = Array.of(1, 4, 5, 7)
+const arr = Array.from('Hello world')
+const listItems = document.querySelectorAll('li')
+const arrayFrom = Array.from(listItems)
+
+console.log(numbers)
+console.log(nums)
+console.log(fixedSizeArray)
+console.log(array)
+console.log(arr)
+console.log(listItems)
+console.log(arrayFrom)
