590faa15d563d534ef09a1206530be1bb5bddd36
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-16 15:15:24 +0100

    add maps

--- section-08-project-01-arrays/setsmaps.js
+++ section-08-project-01-arrays/setsmaps.js
@@ -12,3 +12,27 @@
 //     console.log(entry)
 // }
 
+const person1 = {name:'Alex'}
+const person2 = {name:'Max'}
+
+const personData = new Map([[person1,[{date:'today', price: 10}]]])
+
+personData.set(person2, [{age:26, surname:'Klimchuk'}])
+
+
+console.log(personData)
+console.log(personData.get(person1))
+
+for (const [key, value] of personData.entries()) {
+    console.log(key, value)
+}
+
+for (const key of personData.keys()) {
+    console.log(key)
+}
+
+for (const value of personData.values()) {
+    console.log(value)
+}
+
+console.log(personData.size())
