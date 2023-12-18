973030be70177b3d4155f84a6bc9375d41101567
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-18 17:25:07 +0100

    divide code onto classes

--- section-10-project-01-oop/assets/scripts/app.js
+++ section-10-project-01-oop/assets/scripts/app.js
@@ -1,46 +1,76 @@
-const productList = {
-    products: [
-        { 
-            title: 'A sofa', 
-            imageUrl : 'someUrl', 
-            price: 2000, 
-            description: 'A soft sofa'
-        },
-        { 
-            title: 'A sofa', 
-            imageUrl : 'someUrl', 
-            price: 2000, 
-            description: 'A soft sofa'
-        },
-        { 
-            title: 'A sofa', 
-            imageUrl : 'someUrl', 
-            price: 2000, 
-            description: 'A soft sofa'
-        }
-    ],
-    render() {
-        const renderHook = document.getElementById('app')
-        const productLi = document.getElementById('ul')
-        productLi.className = 'product-list'
-        for (const product of this.products) {
-            const productEl = document.createElement('li')
-            productEl.className = 'product-item'
-            productEl.innerHTML = `
-                <div>
-                    <img src="${product.imageUrl}" alt="${product.title}" >
-                    <div class="product-item__content">
-                        <h2>${product.title}</h2>
-                        <h3>\$${product.price}<h3>
-                        <p>${product.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>        
-            `
-            productLi.append(productEl)
-        }
-        renderHook.append(productLi)
+class Product {
+    title = 'Default'
+    imageUrl
+    description
+    price
+
+    constructor(title, imageUrl, description, price) {
+        this.title = title
+        this.imageUrl = imageUrl
+        this.description = description
+        this.price = price
     }
 }
 
+class ProductItem {
+    constructor(product){
+        this.product = product
+    }
+
+    render(){
+        const productLi = document.createElement('li')
+        productLi.className = 'product-item'
+        productLi.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}<h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>        
+        `
+        return productLi
+    }
+}
+
+class ProductList {
+    products = [
+        new Product(
+            'A sofa', 
+            'https://leatherexpressions.com/wp-content/uploads/2018/09/divani-sofa.jpg', 
+            2399, 
+            'A soft sofa'
+        ),
+        new Product(
+            'A sofa', 
+            'https://leatherexpressions.com/wp-content/uploads/2018/06/image-44.jpg', 
+            5399,
+            'A soft sofa'
+        ),
+        new Product(
+            'A sofa', 
+            'https://leatherexpressions.com/wp-content/uploads/2018/06/image-36.jpg', 
+            3399, 
+            'A soft sofa'
+        )
+    ]
+
+    constructor() {}
+
+    render() {
+        const renderHook = document.getElementById('app')
+        const productUl = document.createElement('ul')
+        productUl.className = 'product-list'
+        for (const product of this.products) {
+            const productItem = new ProductItem(product)
+            const productLi = productItem.render()
+            productUl.append(productLi)
+        }
+        renderHook.append(productUl)
+    }
+}
+
+const productList = new ProductList()
 productList.render()
