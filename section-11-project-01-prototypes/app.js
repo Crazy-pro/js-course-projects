6befafc9846139a9b68a877169aef21abe8225e3
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-20 16:59:46 +0100

    add constructor method body

---
+++ section-11-project-01-prototypes/app.js
@@ -0,0 +1,20 @@
+class Person {
+    name = 'Alex'
+
+    constructor() {
+        this.age = 25
+    }
+
+    greet() {
+        console.log(`Hi there. I\'m ${this.name} I\'m ${this.age} years old.`)
+    }
+}
+
+function Person() {
+    this.name = 'Alex'
+    this.age = 25
+    this.greet = () => console.log(`Hi there. I\'m ${this.name} I\'m ${this.age} years old.`)
+}
+
+const person = new Person()
+person.greet()
