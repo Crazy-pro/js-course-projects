edc20ad16f38935983af37c60a6ce7046581b3be
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-20 17:30:34 +0100

    add prototypes

--- section-11-project-01-prototypes/app.js
+++ section-11-project-01-prototypes/app.js
@@ -1,27 +1,27 @@
-class AgedPerson {
-    printAge() {
-        console.log(`${this.age} years old.`)
-    }
-}
-
-class Person extends AgedPerson {
-    name = 'Alex'
-
-    constructor() {
-        super()
-        this.age = 25
-    }
-
-    greet() {
-        console.log(`Hi there. I\'m ${this.name} I\'m ${this.age} years old.`)
-    }
-}
-
-// function Person() {
-//     this.name = 'Alex'
-//     this.age = 25
-//     this.greet = () => console.log(`Hi there. I\'m ${this.name} I\'m ${this.age} years old.`)
+// class AgedPerson {
+//     printAge() {
+//         console.log(`${this.age} years old.`)
+//     }
 // }
+
+// class Person extends AgedPerson {
+//     name = 'Alex'
+
+//     constructor() {
+//         super()
+//         this.age = 25
+//     }
+
+//     greet() {
+//         console.log(`Hi there. I\'m ${this.name} I\'m ${this.age} years old.`)
+//     }
+// }
+
+function Person() {
+    this.name = 'Alex'
+    this.age = 25
+    this.greet = () => console.log(`Hi there. I\'m ${this.name} I\'m ${this.age} years old.`)
+}
 
 // Person.prototype = {
 //     printAge() {
@@ -31,12 +31,28 @@ class Person extends AgedPerson {
 
 const person = new Person()
 person.greet()
-person.printAge()
+// person.printAge()
+
+Person.prototype.printAge = function () {
+    console.log(`${this.age} years old.`)
+}
+
+Person.describe = function () {
+    console.log('Creating Person...')
+}
 
 console.dir(Person)
+console.dir(Object)
 
-console.log(person.toString())
 console.log(person.__proto__)
+console.log(person.toString())
 console.log(person.__proto__.constructor)
 console.log(person.__proto__.__proto__)
 console.log(person.__proto__ === Person.prototype)
+
+const person2 = new person.__proto__.constructor()
+console.log(person2)
+console.log(person2.length)
+console.log(Object.length)
+
+console.dir(Object.prototype.__proto__)
