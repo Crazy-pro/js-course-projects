1981c08224bde34b2cc67807fdc148d77065f920
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-24 15:35:10 +0100

    add event Listener

---
+++ section-14-project-01-events/assets/scripts/events.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button')
+
+// button.onclick = function () {
+
+// }
+
+// const buttonClickHandler = () => {
+//     alert('Button was clicked!')
+// }
+
+// button.onclick = buttonClickHandler
+
+button.addEventListener('click',)
+
+button.removeEventListener()
