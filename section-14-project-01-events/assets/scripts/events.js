d7d7554dcf3e97a13d7ad82ea5e074f33acc4711
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 00:53:36 +0100

    add stopPropogation()

--- section-14-project-01-events/assets/scripts/events.js
+++ section-14-project-01-events/assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button')
+const button = document.querySelector('button')
 
 const buttonClickHandler = event => {
     // event.target.disabled = true
@@ -32,5 +32,18 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', event => {
     event.preventDefault()
+    console.log(event)
+})
+
+const div = document.querySelector('div')
+
+div.addEventListener('click', event => {
+    console.log('Clicked Div')
+    console.log(event)
+}, true)
+
+button.addEventListener('click', event => {
+    event.stopPropagation()
+    console.log('Clicked Button')
     console.log(event)
 })
