6e569a91f486e74f8f2c77a4274493b89c2398d4
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-24 15:21:43 +0100

    fix tooltip text

--- section-14-project-01-events/assets/scripts/app.js
+++ section-14-project-01-events/assets/scripts/app.js
@@ -31,7 +31,10 @@ class Component {
   }
 
   attach() {
-    this.hostElement.insertAdjacentElement(this.insertBefore ? 'afterbegin' : 'beforeend', this.element)
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    )
   }
 }
 
@@ -56,16 +59,16 @@ class Tooltip extends Component {
     tooltipBody.querySelector('p').textContent = this.text
     tooltipElement.append(tooltipBody)
 
-    const hostElementPosLeft = this.hostElement.offsetLeft
-    const hostElementPosTop = this.hostElement.offsetTop
-    const hostElementHeight = this.hostElement.clientHeight
+    const hostElPosLeft = this.hostElement.offsetLeft
+    const hostElPosTop = this.hostElement.offsetTop
+    const hostElHeight = this.hostElement.clientHeight
     const parentElementScrolling = this.hostElement.parentElement.scrollTop
 
-    const x = hostElementPosLeft + 20
-    const y = hostElementPosTop + hostElementHeight - parentElementScrolling - 10
+    const x = hostElPosLeft + 20
+    const y = hostElPosTop + hostElHeight - parentElementScrolling - 10
 
     tooltipElement.style.position = 'absolute'
-    tooltipElement.style.left = x + 'px'
+    tooltipElement.style.left = x + 'px' // 500px
     tooltipElement.style.top = y + 'px'
 
     tooltipElement.addEventListener('click', this.closeTooltip)
@@ -76,9 +79,9 @@ class Tooltip extends Component {
 class ProjectItem {
   hasActiveTooltip = false
 
-  constructor(id, updateProjectsFunction, type) {
+  constructor(id, updateProjectListsFunction, type) {
     this.id = id
-    this.updateProjectsHandler = updateProjectsFunction
+    this.updateProjectListsHandler = updateProjectListsFunction
     this.connectMoreInfoButton()
     this.connectSwitchButton(type)
   }
@@ -88,30 +91,39 @@ class ProjectItem {
       return
     }
     const projectElement = document.getElementById(this.id)
-    const tooltipText = projectElement.dataset.extrainfo
-    const tooltip = new Tooltip(() => {
-      this.hasActiveTooltip = false
-    }, tooltipText, this.id)
+    const tooltipText = projectElement.dataset.extraInfo
+    const tooltip = new Tooltip(
+      () => {
+        this.hasActiveTooltip = false
+      },
+      tooltipText,
+      this.id
+    )
     tooltip.attach()
     this.hasActiveTooltip = true
   }
 
   connectMoreInfoButton() {
     const projectItemElement = document.getElementById(this.id)
-    const moreInfoButton = projectItemElement.querySelector('button:first-of-type')
-    moreInfoButton.addEventListener('click', this.showMoreInfoHandler.bind(this))
+    const moreInfoBtn = projectItemElement.querySelector(
+      'button:first-of-type'
+    )
+    moreInfoBtn.addEventListener('click', this.showMoreInfoHandler.bind(this))
   }
 
   connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id)
-    let switchButton = projectItemElement.querySelector('button:last-of-type')
-    switchButton = DOMHelper.clearEventListeners(switchButton)
-    switchButton.textContent = type === 'active' ? 'Finish' : 'Activate'
-    switchButton.addEventListener('click', this.updateProjectsHandler.bind(null, this.id))
+    let switchBtn = projectItemElement.querySelector('button:last-of-type')
+    switchBtn = DOMHelper.clearEventListeners(switchBtn)
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate'
+    switchBtn.addEventListener(
+      'click',
+      this.updateProjectListsHandler.bind(null, this.id)
+    )
   }
 
-  update(updateProjectsFunction, type) {
-    this.updateProjectsHandler = updateProjectsFunction
+  update(updateProjectListsFn, type) {
+    this.updateProjectListsHandler = updateProjectListsFn
     this.connectSwitchButton(type)
   }
 }
@@ -121,14 +133,16 @@ class ProjectList {
 
   constructor(type) {
     this.type = type
-    const projectItems = document.querySelectorAll(`#${type}-projects li`)
-    for (const projectItem of projectItems) {
-      this.projects.push(new ProjectItem(projectItem.id, this.switchProject.bind(this)), this.type)
+    const prjItems = document.querySelectorAll(`#${type}-projects li`)
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
+      )
     }
     console.log(this.projects)
   }
 
-  setSwitchHandler(switchHandlerFunction) {
+  setSwitchHandlerFunction(switchHandlerFunction) {
     this.switchHandler = switchHandlerFunction
   }
 
@@ -148,16 +162,20 @@ class ProjectList {
 
 class App {
   static init() {
-    const activeProjects = new ProjectList('active')
-    const finishedProjects = new ProjectList('finished')
-    activeProjects.setSwitchHandler(finishedProjects.addProject.bind(finishedProjects))
-    finishedProjects.setSwitchHandler(activeProjects.addProject.bind(activeProjects))
+    const activeProjectsList = new ProjectList('active')
+    const finishedProjectsList = new ProjectList('finished')
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    )
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    )
 
     const timerId = setTimeout(this.startAnalytics, 3000)
 
-    document
-      .getElementById('stop-analytics-button')
-      .addEventListener('click', () => clearTimeout(timerId))
+    document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+      clearTimeout(timerId)
+    })
   }
 
   static startAnalytics() {
