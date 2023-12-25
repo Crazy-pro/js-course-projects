a9c34f39fec54e44cc5d6ca7deab45a63433d2f7
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 17:50:02 +0100

    add delete function

--- section-18-project-01-http/assets/scripts/app.js
+++ section-18-project-01-http/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts')
 const postTemplate = document.getElementById('single-post')
 const form = document.querySelector('#new-post form')
 const fetchButton = document.querySelector('#available-posts button')
+const postList = document.querySelector('ul')
 
 const sendHttpRequest = (method, url, data) => {
     const promise = new Promise((resolve, reject) => {
@@ -25,6 +26,7 @@ const sendHttpRequest = (method, url, data) => {
 const baseUrl = 'https://jsonplaceholder.typicode.com/'
 const getPostsUrl = baseUrl + 'posts'
 const postPostUrl = baseUrl + 'posts'
+const deletePostUrl = baseUrl + 'posts'
 
 async function fetchPosts() {
     const responseData = await sendHttpRequest('GET', getPostsUrl)
@@ -57,3 +59,9 @@ form.addEventListener('submit', event => {
     createPost(enteredTittle, enteredContent)
 })
 
+postList.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        const postId = event.target.closest('li').id
+        sendHttpRequest('DELETE', deletePostUrl + postId)
+    }
+})
