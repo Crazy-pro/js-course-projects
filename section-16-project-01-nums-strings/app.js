628a193121df1392efe71faaf4b8c479a4ad5496
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 14:58:48 +0100

    add method with template params

--- section-16-project-01-nums-strings/app.js
+++ section-16-project-01-nums-strings/app.js
@@ -4,3 +4,22 @@ function randomIntBetween(min, max) {
 
 console.log(Math.random())
 console.log(randomIntBetween(5, 10))
+
+function productDescription(strings, productName, productPrice) {
+    console.log('strings', strings)
+    console.log(productName)
+    console.log(productPrice)
+    let priceCategory = 'pretty cheap'
+    if (productPrice > 20) {
+        priceCategory = 'fairly priced'
+    }
+    // return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`
+    return { name: productName, price: productPrice }
+}
+
+const productName = 'Course'
+const productPrice = 29.99
+
+const productOutput = productDescription`This product (${productName}) is ${productPrice}.`
+
+console.log(productOutput)
