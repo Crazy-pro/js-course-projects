8abe89db99bcf18283313a32d919910baa397039
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 17:46:39 +0100

    add fetch posts and post button form

--- section-18-project-01-http/assets/scripts/app.js
+++ section-18-project-01-http/assets/scripts/app.js
@@ -1,23 +1,59 @@
-const xmlHttpRequest = new XMLHttpRequest()
+const listElement = document.querySelector('.posts')
+const postTemplate = document.getElementById('single-post')
+const form = document.querySelector('#new-post form')
+const fetchButton = document.querySelector('#available-posts button')
 
-xmlHttpRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts')
+const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise((resolve, reject) => {
+        const xmlHttpRequest = new XMLHttpRequest()
 
-xmlHttpRequest.responseType = 'json'
+        xmlHttpRequest.open(method, url)
 
-xmlHttpRequest.onload = function () {
-    // const posts = JSON.parse(xmlHttpRequest.response)
-    const posts = xmlHttpRequest.response
+        xmlHttpRequest.responseType = 'json'
 
+        xmlHttpRequest.onload = function () {
+            // const posts = JSON.parse(xmlHttpRequest.response)
+            resolve(xmlHttpRequest.response)
+            // console.log(posts)
+        }
+
+        xmlHttpRequest.send(JSON.stringify(data))
+    })
+    return promise
+}
+
+const baseUrl = 'https://jsonplaceholder.typicode.com/'
+const getPostsUrl = baseUrl + 'posts'
+const postPostUrl = baseUrl + 'posts'
+
+async function fetchPosts() {
+    const responseData = await sendHttpRequest('GET', getPostsUrl)
+    const posts = responseData
     for (const post of posts) {
         const postElement = document.importNode(postTemplate.content, true)
         postElement.querySelector('h2').textContent = post.title.toUpperCase()
         postElement.querySelector('p').textContent = post.body
+        postElement.querySelector('li').id = post.id
         listElement.append(postElement)
     }
-    console.log(posts)
 }
 
-xmlHttpRequest.send()
+async function createPost(title, content) {
+    const userId = Math.random()
+    const post = {
+        title: title,
+        body: content,
+        userId: userId
+    }
+    sendHttpRequest('POST', postPostUrl, post)
+}
 
-const listElement = document.querySelector('.posts')
-const postTemplate = document.getElementById('single-post')
+fetchButton.addEventListener('click', fetchPosts)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const enteredTittle = event.currentTarget.querySelector('#title').value
+    const enteredContent = event.currentTarget.querySelector('#content').value
+    createPost(enteredTittle, enteredContent)
+})
+
