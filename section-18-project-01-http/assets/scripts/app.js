510027735c0807531d758b8162bd7ae0ac981731
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 16:57:24 +0100

    add JSON parser

--- section-18-project-01-http/assets/scripts/app.js
+++ section-18-project-01-http/assets/scripts/app.js
@@ -2,4 +2,9 @@ const xmlHttpRequest = new XMLHttpRequest()
 
 xmlHttpRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts')
 
+xmlHttpRequest.onload = function () {
+    const posts = JSON.parse(xmlHttpRequest.response)
+    console.log(posts)
+}
+
 xmlHttpRequest.send()
