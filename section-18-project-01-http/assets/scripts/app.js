09bec4020825c8e5c34b432188020057bf98136a
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 17:03:02 +0100

    parse dummy data

--- section-18-project-01-http/assets/scripts/app.js
+++ section-18-project-01-http/assets/scripts/app.js
@@ -2,9 +2,22 @@ const xmlHttpRequest = new XMLHttpRequest()
 
 xmlHttpRequest.open('GET', 'https://jsonplaceholder.typicode.com/posts')
 
+xmlHttpRequest.responseType = 'json'
+
 xmlHttpRequest.onload = function () {
-    const posts = JSON.parse(xmlHttpRequest.response)
+    // const posts = JSON.parse(xmlHttpRequest.response)
+    const posts = xmlHttpRequest.response
+
+    for (const post of posts) {
+        const postElement = document.importNode(postTemplate.content, true)
+        postElement.querySelector('h2').textContent = post.title.toUpperCase()
+        postElement.querySelector('p').textContent = post.body
+        listElement.append(postElement)
+    }
     console.log(posts)
 }
 
 xmlHttpRequest.send()
+
+const listElement = document.querySelector('.posts')
+const postTemplate = document.getElementById('single-post')
