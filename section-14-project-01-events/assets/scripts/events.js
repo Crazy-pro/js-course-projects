d7d1795753a789f12da3f42b286810a1687a7531
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 01:20:36 +0100

    add closest element event listeners

--- section-14-project-01-events/assets/scripts/events.js
+++ section-14-project-01-events/assets/scripts/events.js
@@ -37,13 +37,29 @@ form.addEventListener('submit', event => {
 
 const div = document.querySelector('div')
 
-div.addEventListener('click', event => {
+div.addEventListener('mouseenter', event => {
     console.log('Clicked Div')
     console.log(event)
-}, true)
+}/*, true*/)
 
-button.addEventListener('click', event => {
+button.addEventListener('mouseenter', event => {
     event.stopPropagation()
     console.log('Clicked Button')
     console.log(event)
+})
+
+
+// const listItems = document.querySelectorAll('li')
+
+// listItems.forEach(listItem => {
+//     listItem.addEventListener('click', event => {
+//         event.target.classList.toggle('highlight')
+//     })
+// });
+
+const list = document.querySelector('ul')
+list.addEventListener('click', event => {
+    console.log(event.currentTarget)
+    // event.target.classList.toggle('highlight')
+    event.target.closest('li').classList.toggle('highlight')
 })
