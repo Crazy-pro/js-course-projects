162e5fc7b007d90d4a4317de5e9373b0769aafdb
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 02:21:37 +0100

    finish drag and drop functionality

--- section-13-project-01-advanced-dom/assets/scripts/app.js
+++ section-13-project-01-advanced-dom/assets/scripts/app.js
@@ -98,9 +98,14 @@ class ProjectItem {
   }
 
   connectDrag() {
-    document.getElementById(this.id).addEventListener('dragstart', event => {
+    const item = document.getElementById(this.id)
+    item.addEventListener('dragstart', event => {
       event.dataTransfer.setData('text/plain', this.id)
       event.dataTransfer.effectAllowed = 'move'
+    })
+
+    item.addEventListener('dragend', event => {
+      console.log(event)
     })
   }
 
@@ -134,6 +139,46 @@ class ProjectList {
       this.projects.push(new ProjectItem(projectItem.id, this.switchProject.bind(this), this.type))
     }
     console.log(this.projects)
+    this.connectDraggable()
+  }
+
+
+  connectDraggable() {
+    const list = document.querySelector(`#${this.type}-projects ul`)
+
+    list.addEventListener('dragenter', event => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault()
+      }
+      list.parentElement.classList.add('droppable')
+    })
+
+    list.addEventListener('dragover', event => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault()
+      }
+    })
+
+    list.addEventListener('dragleave', event => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove('droppable')
+      }
+    })
+
+    list.addEventListener('drop', event => {
+      const projectId = event.dataTransfer.getData('text/plain')
+      if (this.projects.find(p => p.id === projectId)) {
+        return
+      }
+
+      document
+        .getElementById(projectId)
+        .querySelector('button:last-of-type')
+        .click()
+
+      list.parentElement.classList.remove('droppable')
+      // event.preventDefault()
+    })
   }
 
   setSwitchHandler(switchHandlerFunction) {
