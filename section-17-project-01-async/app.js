330ab8a7d33c4ec5fe488f891eaad828e6eefa97
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-25 15:29:11 +0100

    event loop basics

--- section-17-project-01-async/app.js
+++ section-17-project-01-async/app.js
@@ -2,15 +2,28 @@ const button = document.querySelector('button')
 const output = document.querySelector('p')
 
 function trackUserHandler() {
-  console.log('Clicked!')
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      setTimeout(() => {
+        console.log('Position: ', position)
+      }, 2000)
+      console.log('Position: ', position)
+    }, error => {
+      console.log('Error: ', error)
+    })
+
+  setTimeout(() => {
+    console.log('Timer is done')
+  }, 0)
+  console.log('Getting Position...')
 }
 
 button.addEventListener('click', trackUserHandler)
 
-let result = 0
+// let result = 0
 
-for (let i = 0; i < 100000; i++) {
-  result += i
-}
+// for (let i = 0; i < 100000; i++) {
+//   result += i
+// }
 
-console.log(result)
+// console.log(result)
