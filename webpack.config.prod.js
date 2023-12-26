c9463aa5c574b9b638e56e42a2b2741530b63472
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-26 19:12:26 +0100

    add productions binding scripts

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,15 @@
 const path = require('path')
-const { CleanPlugin } = require('webpack')
 const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './section-21-project-01-tools/src/app.js',
     output: {
-        filename: 'app.js',
+        filename: '[contenthash].js',
         path: path.resolve(__dirname, 'assets', 'scripts'),
         publicPath: 'assets/scripts/'
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-source-map',
     plugins: [
         new CleanPlugin.CleanWebpackPlugin()
     ]
