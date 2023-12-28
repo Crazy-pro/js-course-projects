6247cbb22c4ed28411a54ef31bf7d65367d20b0b
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-29 00:04:07 +0100

    add cookies example

--- section-22-project-01-storage/app.js
+++ section-22-project-01-storage/app.js
@@ -1,26 +1,21 @@
 const storeButton = document.getElementById('store-btn')
 const retrieveButton = document.getElementById('retrieve-btn')
 
-const userId = 'u123'
-
-const user = {
-    name: 'Max',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
-}
-
 storeButton.addEventListener('click', () => {
-    localStorage.setItem('uid', userId)
-    localStorage.setItem('user', JSON.stringify(user))
+    const userId = 'u123'
+    const user = {
+        name: 'Max',
+        age: 30,
+        hobbies: ['Sports', 'Cooking']
+    }
+    document.cookie = `uid=${userId}; max-age=360`
+    document.cookie = `user=${JSON.stringify(user)}`
 })
 
 retrieveButton.addEventListener('click', () => {
-    const extractedId = localStorage.getItem('uid')
-    const extractedUser = JSON.parse(localStorage.getItem('user'))
-    if (extractedId) {
-        console.log('Got the id - ' + extractedId)
-        console.log('Got the user - ' + extractedUser)
-    } else {
-        console.log('Couldn\'t find the id...')
-    }
+    console.log(document.cookie)
+    const cookieData = document.cookie.split(';')
+    const data = cookieData.map(i => i.trim())
+    console.log(data[1].split('=')[1])
 })
+
---
+++ section-22-project-01-storage/localStorage.js
@@ -0,0 +1,26 @@
+const storeButton = document.getElementById('store-btn')
+const retrieveButton = document.getElementById('retrieve-btn')
+
+const userId = 'u123'
+
+const user = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+}
+
+storeButton.addEventListener('click', () => {
+    sessionStorage.setItem('uid', userId)
+    localStorage.setItem('user', JSON.stringify(user))
+})
+
+retrieveButton.addEventListener('click', () => {
+    const extractedId = sessionStorage.getItem('uid')
+    const extractedUser = JSON.parse(localStorage.getItem('user'))
+    if (extractedId) {
+        console.log('Got the id - ' + extractedId)
+        console.log('Got the user - ' + extractedUser)
+    } else {
+        console.log('Couldn\'t find the id...')
+    }
+})
