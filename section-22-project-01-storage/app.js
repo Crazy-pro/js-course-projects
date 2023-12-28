0c1379e2678b7bea96d7637a3c4a2c239cae58c9
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-28 23:50:34 +0100

    parse object to JSON

--- section-22-project-01-storage/app.js
+++ section-22-project-01-storage/app.js
@@ -11,13 +11,15 @@ const user = {
 
 storeButton.addEventListener('click', () => {
     localStorage.setItem('uid', userId)
-    localStorage.setItem('user', user)
+    localStorage.setItem('user', JSON.stringify(user))
 })
 
 retrieveButton.addEventListener('click', () => {
     const extractedId = localStorage.getItem('uid')
+    const extractedUser = JSON.parse(localStorage.getItem('user'))
     if (extractedId) {
         console.log('Got the id - ' + extractedId)
+        console.log('Got the user - ' + extractedUser)
     } else {
         console.log('Couldn\'t find the id...')
     }
