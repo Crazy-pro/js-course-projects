f74c662ff09d2d1aa6cbc3af00cf03530058f774
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-29 00:11:42 +0100

    add indexDB

--- section-22-project-01-storage/cookies.js
+++ section-22-project-01-storage/cookies.js
@@ -1,29 +1,6 @@
 const storeButton = document.getElementById('store-btn')
 const retrieveButton = document.getElementById('retrieve-btn')
 
-const dbRequest = indexedDB.open('StorageDummy', 1)
-
-dbRequest.onupgradeneeded = function (event) {
-    const db = event.target.result
-
-    const objectStore = db.createObjectStore('products', { keyPath: 'id' })
-
-    objectStore.transaction.oncomplete = function (event) {
-        const productsStore = db.transaction('products', 'readwrite')
-            .objectStore('products')
-        productsStore.add({
-            id: 'p1',
-            title: 'A first Product',
-            price: 12.99,
-            tags: ['Expensive', 'Luxury']
-        })
-    }
-}
-
-dbRequest.onerror = function (event) {
-    console.log('Error!')
-}
-
 storeButton.addEventListener('click', () => {
     const userId = 'u123'
     const user = {
