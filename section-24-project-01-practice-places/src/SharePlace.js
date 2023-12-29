52525c2b216df23a493a8ac06caf7500df9280d2
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-29 16:16:19 +0100

    add fix

--- section-24-project-01-practice-places/src/SharePlace.js
+++ section-24-project-01-practice-places/src/SharePlace.js
@@ -1,36 +1,36 @@
 class PlaceFinder {
   constructor() {
-    const addressForm = document.querySelector('form');
-    const locateUserBtn = document.getElementById('locate-btn');
+    const addressForm = document.querySelector('form')
+    const locateUserButton = document.getElementById('locate-btn')
 
-    locateUserBtn.addEventListener('click', this.locateUserHandler);
-    addressForm.addEventListener('submit', this.findAddressHandler);
+    locateUserButton.addEventListener('click', this.locateUserHandler)
+    addressForm.addEventListener('submit', this.findAddressHandler)
   }
 
   locateUserHandler() {
     if (!navigator.geolocation) {
       alert(
         'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
-      );
-      return;
+      )
+      return
     }
     navigator.geolocation.getCurrentPosition(
       successResult => {
         const coordinates = {
           lat: successResult.coords.latitude + Math.random() * 50,
           lng: successResult.coords.longitude + Math.random() * 50,
-        };
-        console.log(coordinates);
+        }
+        console.log(coordinates)
       },
       error => {
         alert(
           'Could not locate you unfortunately. Please enter an address manually!'
-        );
+        )
       }
-    );
+    )
   }
 
   findAddressHandler() { }
 }
 
-const placeFinder = new PlaceFinder();
+const placeFinder = new PlaceFinder()
