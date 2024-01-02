63d151c0e23d7b25f264e6348bd071312baa533c
Crazy-pro <Crazy.pro.noob@gmail.com>
2024-01-02 19:07:31 +0100

    add Reflection example

--- section-26-project-01-meta/app.js
+++ section-26-project-01-meta/app.js
@@ -19,3 +19,92 @@ user.id = 'p2' // this should not be possible!
 console.log(user[Symbol('uid')])
 console.log(Symbol('uid') === Symbol('uid'))
 console.log(user.toString())
+
+const company = {
+  currentEmployee: 0,
+  employees: ['Alex', 'Andrew', 'Ann'],
+  next() {
+    if (this.currentEmployee >= this.employees.length) {
+      return {
+        value: this.currentEmployee,
+        done: true
+      }
+    }
+
+    const returnValue = {
+      value: this.employees[this.currentEmployee],
+      done: false
+    }
+    this.currentEmployee++
+    return returnValue
+  },
+  // getEmployee: function* employeeGenerator() {
+  //   // let employee = company.next()
+
+  //   // while (!employee.done) {
+  //   //   yield employee.value
+  //   //   employee = company.next()
+  //   // }
+  //   let currentEmployee = 0
+  //   while (!currentEmployee < this.employees.length) {
+  //     yield this.employees[currentEmployee]
+  //     currentEmployee++
+  //   }
+  // },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next()
+
+    // while (!employee.done) {
+    //   yield employee.value
+    //   employee = company.next()
+    // }
+    let currentEmployee = 0
+    while (!currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee]
+      currentEmployee++
+    }
+  }
+}
+
+// let employee = company.next()
+
+// while (!employee.done) {
+//   console.log(employee.value)
+//   employee = company.next()
+// }
+
+// for (const employee of company) {
+//   console.log(employee)
+// }
+
+// console.log([...company])
+
+// const it = company.getEmployee()
+
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+
+const persons = ['Alex', 'Ann']
+console.log(persons)
+
+// ----
+
+const course = {
+  title: 'JavaScript - Course'
+}
+
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title
+  }
+})
+
+// Reflect.deleteProperty(course, 'title')
+
+// Object.deleteProperty(course, 'title')
+
+// delete course.title
+
+console.log(course.toString())
